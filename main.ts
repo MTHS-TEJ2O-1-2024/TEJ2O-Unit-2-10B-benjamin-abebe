97b2256b9eb802c24cf117fdb139bb553a82bd1b
benjamin-abebe <benjamin.abebe@mths.ca>
2024-10-15 11:23:22 -0400

    Made change on the typescript version.

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Benjamin Abebe
+ * Created on: Oct 2024
+ * This program reads in the amount of light
 */
 
-basic.showString('Hello, World!')
+let neopixelStrip: neopixel.Strip = null
+let lightLevel = input.lightLevel()
+
+input.onButtonPressed(Button.A, function() {
+    if (lightLevel <=51) {
+        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.showIcon(IconNames.Happy)
+    }
+    
+    if (lightLevel)
+})
