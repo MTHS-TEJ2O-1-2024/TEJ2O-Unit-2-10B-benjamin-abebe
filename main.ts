6e84d87e09b2ec66a6ac51c6d28142bbd76d186f
benjamin-abebe <benjamin.abebe@mths.ca>
2024-10-15 23:27:06 -0400

    Made small change on the typescript file.

--- main.ts
+++ main.ts
@@ -5,48 +5,47 @@
  * This program reads in the amount of light
 */
 
-let neopixelStrip: neopixel.Strip = null
-let lightLevel = input.lightLevel()
+let neopixelStrip: neopixel.Strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
-// clear screen and show happy face(-__-)
+// clear screen and show happy face
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 // when button A is pressed
-input.onButtonPressed(Button.A, function() {
-    if (lightLevel <=51) {
-        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.show()
-        basic.showIcon(IconNames.Happy)
+input.onButtonPressed(Button.A, function () {
+    // Read the current light level each time the button is pressed
+    let lightLevel = input.lightLevel()
+
+    // Clear all pixels to black initially
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+
+    if (lightLevel <= 51) {
+        // Do nothing, all pixels remain off (already set to black)
     }
-     
+
     if (lightLevel > 52) {
         neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
     }
 
     if (lightLevel > 104) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
         neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
     }
 
     if (lightLevel > 156) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
         neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
     }
 
     if (lightLevel > 208) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
         neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
     }
 
-    // clear screen and show happy face
+    // Show the updated NeoPixels
+    neopixelStrip.show()
+
+    // Clear screen and show happy face
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
 })
